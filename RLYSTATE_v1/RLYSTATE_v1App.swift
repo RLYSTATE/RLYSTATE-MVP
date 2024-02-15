55681eb66a24dd5ac22486714e20486023b5c0dd
shervin <shervinmobasheri@yahoo.com>
2024-02-15 18:58:18 -0500

    App Check

--- RLYSTATE_v1/RLYSTATE_v1App.swift
+++ RLYSTATE_v1/RLYSTATE_v1App.swift
@@ -7,13 +7,22 @@
 
 // RLYSTATE_v1App.swift
 import SwiftUI
+import UIKit
 import Firebase
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct Rlystate_V2App: App {
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
