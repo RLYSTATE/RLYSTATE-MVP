a229d57cb3a8a22fc244023f4574910f53405afe
smobash <shervin@rlystate.com>
2025-03-10 18:25:16 -0400

    updated notifications

---
+++ RLYSTATE_v1/View/Comments/CommentWrapperView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentWrapperView.swift
+//  RLYSTATE_v1
+//
+//  Created by Shervin Mobasheri on 4/5/24.
+//
+
+import SwiftUI
+
+struct CommentWrapperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentWrapperView()
+}
